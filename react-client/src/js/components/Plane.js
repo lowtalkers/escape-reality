a205a1003efe15039ec768fa0a4bc038c31052ed
Franco Abaroa <franco9395@gmail.com>
2016-11-14 12:03:32 -0800

    Sample card placement for Union Square Image

--- react-client/src/js/components/Plane.js
+++ react-client/src/js/components/Plane.js
@@ -2,9 +2,10 @@ import {Entity} from 'aframe-react';
 import React from 'react';
 
 export default props => (
-  <Entity 
-    geometry="primitive: plane; width: 2; height: 2" 
-    position="0 1 -3"
-    rotation="-10 0 0" />
+  <Entity
+    geometry="primitive: plane; width: 2; height: 2"
+    position="-1.12 5.12 3.02"
+    rotation="-20 80 0"
+    visible="true"/>
 );
 
