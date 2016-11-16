2abea1f90e2875ccf75347fb6d2593cd0c9a9858
Carlos Portillo <caportil@gmail.com>
2016-11-16 01:07:58 -0800

    Add html scrub function to API call to properly render wiki information in TextPlanes

--- server/server.js
+++ server/server.js
@@ -6,12 +6,15 @@ const port = 3000;
 
 /** WIKIPEDIA API QUERY (start) **/
 
-// var sampleSearch = "Macy's";
+
 var routes = ['/', '/sf', '/lobby', '/louvre', '/berlin'];
 
 routes.forEach(function(route) {
   app.use(route, express.static(__dirname + '/../react-client'))
 });
+
+var sampleSearch = "Normandy_landings";
+
 
 // request('http://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=' + sampleSearch + '&format=json&exintro=1', (err, res, body) => {
 //   if (err) {
@@ -19,7 +22,16 @@ routes.forEach(function(route) {
 //   } else {
 //     var query = (JSON.parse(body)).query.pages;
 //     var text = query[(Object.keys(query)[0])].extract;
-//     console.log(text);
+//     console.log('Unscrubbed text:', text);
+
+//     let regex = /(<([^>]+)>)/ig;
+//     // body = `<p>The <b>Louvre</b> or the <b>Louvre Museum</b> (French: <span lang=\"fr\" xml:lang=\"fr\"><i>Musée du Louvre</i></span>, <small>pronounced: </small><span title=\"Representation in the International Phonetic Alphabet (IPA)\">[myze dy luvʁ]</span>) (<small>French</small> <span><span><span><span> </span></span> </span></span>) is the world's largest museum and a historic monument in Paris, France. A central landmark of the city, it is located on the Right Bank of the Seine in the city's 1st arrondissement (district or ward). Nearly 35,000 objects from prehistory to the 21st century are exhibited over an area of 72,735 square metres (782,910 square feet). The Louvre is the world's second most visited museum after the Palace Museum in China, receiving more than 9.26 million visitors in 2014.</p>`;
+//     result = text.replace(regex, "");
+//     let regexApostrophes = /(\')/ig;
+//     let regexNewlines = /(\n)/ig;
+//     let output = result.replace(regexApostrophes, "'").replace(regexNewlines, "");
+//     console.log('Scrubbed output is:', output);
+//     return output;
 //   }
 // });
 
