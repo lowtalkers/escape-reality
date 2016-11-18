37b5e261978feb1959c98b20360f859e6bbc8094
Mach13 <amad_khan1@hotmail.com>
2016-11-17 19:54:01 -0800

    Setup authentication

---
+++ db/models/index.js
@@ -0,0 +1,30 @@
+// This file makes all join table relationships
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('escape', 'escape', 'escapeacft', {
+  dialect: 'mariadb',
+  host: 'localhost'
+});
+
+// Any vairable that starts with a capital letter is a model
+var User = require('./users.js')(sequelize, Sequelize);
+// var BookmarkUsers = require('./bookmarkUsers')(sequelize, Sequelize);
+
+// BookmarkUsers join table:
+// User.belongsToMany(Bookmark, {
+//   through: 'bookmark_users',
+//   foreignKey: 'user_id'
+// });
+
+// Bookmark.belongsToMany(User, {
+//   through: 'bookmark_users',
+//   foreignKey: 'bookmark_id'
+// });
+
+
+// sequelize.sync({force: true});
+sequelize.sync();
+
+exports.User = User;
+// exports.Bookmark = Bookmark;
+
+// exports.BookmarkUsers = BookmarkUsers;
