d0d0de3188eb7bf6154f87af1f1346f9ec2190d0
Carlos Portillo <caportil@gmail.com>
2016-11-17 16:57:41 -0800

    Render complete TextPlane 0.1 Z degrees in front of tag for placement purposes

--- react-client/src/components/Louvre.js
+++ react-client/src/components/Louvre.js
@@ -21,6 +21,17 @@ import Bookmark from './Bookmark';
 // AFRAME.registerComponent("textwrap", textwrap);
 // let show = false;
 
+/*
+not sure how these 'dumb' components will handle the equivalent of
+state changes once imported into our smart component?
+i.e onClick -> this.setState({louvrePyramid: true}) in order to
+determine whether to render Louvre Pyramid etc. obv we can do it
+differently but that's how I've always rendered new components in React before
+*/
+
+//temporary workaround for now
+let showPyramid = false;
+
 export default props => (
       <Entity >
 
@@ -32,44 +43,21 @@ export default props => (
           <Entity template="src: #link" data-src="#sechelt" data-thumb="#sechelt-thumb"></Entity>
         </Entity>
 
-        <Entity geometry="primitive: plane; width: 2; height: 2"
+        <Entity id="pyramidTag" 
+            onClick={() => {
+              if (showPyramid) {};
+              showPyramid = !showPyramid;
+            }}
+            geometry="primitive: plane; width: 2; height: 2"
             material={{src: 'url(https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-cubes.jpg)', opacity: 0.85}}
             position="-4.34 2.22 6.83"
-            rotation="8.02 135.22 -1.15">
+            rotation="8.02 135.22 -1.15"
+        >
         </Entity>
-
-
-
-        <Entity geometry="primitive: plane; width: 2; height: 2"
-            onClick={() => console.log('The Louvre Museum in Paris, France')}
-            material={{color: 'yellow', opacity: 0.35}}
-            position="-8.02 2.97 -3.8"
-            rotation="-2.86 53.86 2.86">
-        </Entity>
-
-        <Entity geometry="primitive: plane; width: 5; height: 5"
-            onClick={() => console.log('The Napoleonic Courtyard in Paris, France')}
-            material={{color: 'yellow', opacity: 0.35}}
-            position="6.4 -4 0.11"
-            rotation="-89.46 -89.95 85">
-        </Entity>
-
-        <Entity geometry="primitive: plane; width: 2; height: 2"
-            draw="text: Hello"
-            onClick={() => props.changePlaneColor()}
-            material={{color: props.planeColor[props.colorIndex], opacity: 0.25}}
-            position="2.32 3.77 8.53"
-            rotation="20.63 -170.74 -6.88">
-        </Entity>
-
-        <TextPlane
-
-          //should maintain a 2:1 width to height ratio for optimal output
-          height= '3'
-          width= '6'
-
-          position="0 0.5 -3"
-          rotation='0 0 0'
+        
+        <TextPlane id="pyramidTextPlane"
+          position="-4.34 2.22 6.83"
+          rotation="8.02 135.22 -1.15"
           scale='0 0 0'
 
           header='Louvre Pyramid'
@@ -77,8 +65,81 @@ export default props => (
           text='The Louvre Pyramid (Pyramide du Louvre) is a large glass and metal pyramid designed by Chinese-American architect I.M. Pei, surrounded by three smaller pyramids, in the main courtyard (Cour Napoléon) of the Louvre Palace (Palais du Louvre) in Paris. The large pyramid serves as the main entrance to the Louvre Museum. Completed in 1989, it has become a landmark of the city of Paris.'
           textAdjust='-1' //lower moves this down, higher moves this up
           imageSrc='https://upload.wikimedia.org/wikipedia/en/thumb/4/42/Louvre_Pyramid.jpg/1024px-Louvre_Pyramid.jpg'
-
         />
+
+{/*
+        <TextPlane id="pyramidTextPlane"
+        //replace two below lines with corresponding tag properties
+          position="-4.34 2.22 6.83"
+          rotation="8.02 135.22 -1.15"
+
+          scale='0 0 0'
+          header='Louvre Pyramid'
+          headerAdjust='-1.5' // lower moves it to the left, higher to the right
+          text='The Louvre Pyramid (Pyramide du Louvre) is a large glass and metal pyramid designed by Chinese-American architect I.M. Pei, surrounded by three smaller pyramids, in the main courtyard (Cour Napoléon) of the Louvre Palace (Palais du Louvre) in Paris. The large pyramid serves as the main entrance to the Louvre Museum. Completed in 1989, it has become a landmark of the city of Paris.'
+          textAdjust='-1' //lower moves this down, higher moves this up
+          imageSrc='https://upload.wikimedia.org/wikipedia/en/thumb/4/42/Louvre_Pyramid.jpg/1024px-Louvre_Pyramid.jpg'
+        />
+*/}
+
+        <Entity id="leftWingTag" 
+            geometry="primitive: plane; width: 2; height: 2"
+            onClick={() => console.log('The Louvre Museum in Paris, France')}
+            material={{color: 'yellow', opacity: 0.35}}
+            position="-8.02 2.97 -3.8"
+            rotation="-2.86 53.86 2.86">
+        </Entity>
+
+        <Entity id='courtyardTag' 
+            geometry="primitive: plane; width: 5; height: 5"
+            onClick={() => console.log('The Napoleonic Courtyard in Paris, France')}
+            material={{color: 'yellow', opacity: 0.35}}
+            position="6.4 -4 0.11"
+            rotation="-89.46 -89.95 85">
+        </Entity>
+
+        <Entity id="richelieuWingTag" 
+            geometry="primitive: plane; width: 2; height: 2"
+            draw="text: Hello"
+            onClick={() => props.changePlaneColor()}
+            material={{color: props.planeColor[props.colorIndex], opacity: 0.25}}
+            position="2.32 3.77 8.53"
+            rotation="20.63 -170.74 -6.88"
+            look-at="[camera]"
+            // lookAt={[camera]}
+        >
+        </Entity>
+
+
+        <Entity id="richelieuWingTag" 
+            geometry="primitive: plane; width: 2; height: 2"
+            draw="text: Hello"
+            onClick={() => props.changePlaneColor()}
+            material={{color: props.planeColor[props.colorIndex], opacity: 0.25}}
+            position="2.32 3.77 8.53"
+            rotation="20.63 -170.74 -6.88"
+            look-at="[camera]"
+            // lookAt={[camera]}
+        >
+
+        </Entity>
+
+
+
+{/*}
+        <TextPlane id="museumTextPlane"
+          //1.78 -1 1.95
+          position="1.75 0.5 3"
+          rotation='0 -177 0'
+          scale='0 0 0'
+
+          header='Louvre Museum'
+          headerAdjust='-1.5' // lower moves it to the left, higher to the right
+          text= {`The Louvre or the Louvre Museum (French: Musée du Louvre, pronounced: [myze dy luvʁ]) (French   ) is the world's largest museum and a historic monument in Paris, France. A central landmark of the city, it is located on the Right Bank of the Seine in the city's 1st arrondissement (district or ward). Nearly 35,000 objects from prehistory to the 21st century are exhibited over an area of 72,735 square metres (782,910 square feet). The Louvre is the world's second most visited museum after the Palace Museum in China, receiving more than 9.26 million visitors in 2014.`}
+          textAdjust='-1' //lower moves this down, higher moves this up
+          imageSrc='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Le_Louvre_-_Aile_Richelieu.jpg/800px-Le_Louvre_-_Aile_Richelieu.jpg '
+        />
+*/}
 
         <a-text color="white" text="Testing A Frame Tag" position="0 0 -3"/>
 
