1b142688fd8493523d746ae42d2990438b693082
spiritphyz <tonyle@ocf.berkeley.edu>
2016-11-18 16:04:26 -0800

    Handle bad title requests on getWiki endpoint

--- server/lib/utilities.js
+++ server/lib/utilities.js
@@ -5,15 +5,23 @@ const Entities = require('html-entities').AllHtmlEntities;
 const entities = new Entities(); // decode strings like '&amp;'
 
 module.exports.fetchWiki = function(req, res) {
+  console.log('🍊  Starting Wikipedia API request for ', req.query.exactWikiTitle);
   const url = 'http://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles='
               + req.query.exactWikiTitle 
               + '&format=json&exintro=1';
-              
+
   request(url, (err, requestResponse, body) => {
     if (err) {
       console.log('Error in Wikipedia fetch', err);
     } else {
       const query = (JSON.parse(body)).query.pages;
+      
+      if (query['-1']) {
+        console.log('🍊  bad title for Wikipedia API request:', query['-1'].title);
+        res.status(404).send('Not Found');
+        return;
+      }
+
       const text = query[(Object.keys(query)[0])].extract;
       const regex = /(<([^>]+)>)/ig;
       const firstParagraph = text.slice(0, text.indexOf('\n'));
@@ -22,7 +30,7 @@ module.exports.fetchWiki = function(req, res) {
       const regexApostrophes = /(\')/ig;
       let output = result.replace(regexApostrophes, '\'');
       output = entities.decode(output);
-      console.log('Sending scrubbed wiki text:', output);
+      console.log('🍊  Sending scrubbed text to client:', output.slice(0, 55) + '...');
 
       res.status(200).send(JSON.stringify(output));
     }
