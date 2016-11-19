956d07dde4ebfd8f97c0d2fd1e1d72942bca6f84
spiritphyz <tonyle@ocf.berkeley.edu>
2016-11-19 14:27:21 -0800

    Change RingTag look to diamond, animate it slowly

--- react-client/src/components/RingTag.js
+++ react-client/src/components/RingTag.js
@@ -3,24 +3,38 @@ import React from 'react';
 import 'aframe-bmfont-text-component';
 
 export default props => {
-  return <Entity id="RingTag" scale='0.5 0.5 0.5'
-    onClick={() => props.clickFunction()}
-    position={props.position}
-    rotation={props.rotation}
+  return ( 
+    <Entity id='RingTag' scale='.5 .5 .5'
+      onClick={() => props.clickFunction()}
+      position={props.position}
+      rotation={props.rotation}
     >
-    <Entity
-      geometry="primitive: ring;"
-      position='0 0 0'
-      rotation='0 0 0'
-      material={{color: props.color}}
-      >
+      <Entity
+        id='cube1RingTag'
+        primitive='a-octahedron'
+        animation__rot={{property: 'rotation', dir: 'alternate', dur: 6000, loop: 'repeat', to: '0 0 360'}}
+        animation__opac={{property: 'opacity', dir: 'alternate', dur: 3000, loop: 'repeat', from: .2, to: .5}}
+        animation__scale={{property: 'scale', dir: 'alternate', dur: 4000, loop: false, to: '4 4 4'}}
+        position='0 0 0'
+        rotation='45 0 45'
+        easing='easeInOutQuad'
+        material={{color: '#61ffff', opacity: 0.6}}
+        >
+      </Entity>
+      <Entity
+        id='cube2RingTag'
+        primitive='a-octahedron'
+        animation__rot={{property: 'rotation', dir: 'alternate', dur: 6000, loop: true, to: '0 360 0'}}
+        animation__opac={{property: 'opacity', dir: 'alternate', dur: 5000, loop: 'repeat', from: .1, to: .7}}
+        animation__scale={{property: 'scale', dir: 'alternate', dur: 4000, times: 2, loop: false, to: '4 4 4'}}
+        position='0 0 0'
+        rotation='45 45 0'
+        easing='easeInOutQuad'
+        material={{color: '#86ffff', opacity: 0.6}}
+        >
+      </Entity>
     </Entity>
-    <Entity id="transparentInnerRing"
-      geometry="primitive: circle;"
-      position='0 0 0'
-      rotation='0 0 0'
-      material={{opacity: 0}}
-      >
-    </Entity>
-  </Entity>
-}
+  );
+};
+
+
