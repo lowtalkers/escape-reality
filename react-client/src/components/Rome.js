4c3c81429a6dcd6e7220a78097aab19df28e2897
spiritphyz <tonyle@ocf.berkeley.edu>
2016-11-19 14:27:50 -0800

    Move close button to side for less click conflicts

--- react-client/src/components/Rome.js
+++ react-client/src/components/Rome.js
@@ -43,8 +43,7 @@ class Rome extends React.Component {
     const self = this;
     return (
       <Entity>
-          <a-image id="close-image" src="#close" geometry="height: 0.3; width: 0.3" position="0 0 -2" onClick={() => self.props.router.replace('/lobby')}></a-image>
-
+          <a-image id="close-image" src="#close" geometry="height: 0.3; width: 0.3" position="2.7 0.7 -0.4" rotation="0 -90 0" onClick={() => self.props.router.replace('/lobby')}></a-image>
           <RingTag id='sant-agnese-church-tag'
             clickFunction={() => {
               self.setState({showChurch: true});
