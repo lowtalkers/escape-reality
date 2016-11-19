0551c62f2701d8df75e4f98fb128d05b9d3cf09c
spiritphyz <tonyle@ocf.berkeley.edu>
2016-11-18 19:33:44 -0800

    3 dynamic paragraphs added to Rome.js

--- react-client/src/components/Rome.js
+++ react-client/src/components/Rome.js
@@ -71,33 +71,72 @@ class Rome extends React.Component {
               textAdjust='0' //lower moves this down, higher moves this up
               imageSrc='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Sant%27Agnese_in_Agone_%28Piazza_Navona%29_September_2015-1.jpg/1024px-Sant%27Agnese_in_Agone_%28Piazza_Navona%29_September_2015-1.jpg'
             />
-
-            :
-
-            null
+            : null
           }
 
         <Entity
             id='fountain-of-4-rivers-tag'
+              onClick={() => {
+                self.setState({show4RiversFountain: true});
+              }}
             geometry="primitive: plane; width: 2; height: 2"
             material={{color: 'red', opacity: 0.25}}
             position="-2.74 4.84 -22.5"
             rotation="0 0 0">
         </Entity>
 
+        {self.state.show4RiversFountain ? 
+          <TextPlane 
+            id="Fountain4RiversTextPlane"
+            hidePlane={() => self.setState({show4RiversFountain: false})}
+
+            position="-2. 1.96 -2.7"
+            rotation="0 49.6 0"
+            
+            scale='0 0 0'
+            header='Fountain of 4 Rivers'
+            headerAdjust='-1.5' // lower moves it to the left, higher to the right
+            text={this.state.allParagraphs['Fontana_dei_Quattro_Fiumi']}
+            textAdjust='0' //lower moves this down, higher moves this up
+            imageSrc='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Fontana_dei_Quattro_Fiumi_%28Piazza_Navona%29_September_2015-1.jpg/1024px-Fontana_dei_Quattro_Fiumi_%28Piazza_Navona%29_September_2015-1.jpg'
+          />
+          : null
+        }
+
         <Entity
             id='moor-fountain-tag'
+              onClick={() => {
+                self.setState({showMoorFountain: true});
+              }}
             geometry="primitive: plane; width: 2; height: 2"
             material={{color: 'red', opacity: 0.25}}
             position="0.53 2.76 20.73"
             rotation="0 -180 0">
         </Entity>
 
-      <a-sky id="image-360" radius="30" src='#rome'></a-sky>
+        {self.state.showMoorFountain ? 
+          <TextPlane 
+            id="MoorFountainTextPlane"
+            hidePlane={() => self.setState({showMoorFountain: false})}
 
-      <Entity light={{type: 'ambient', color: '#888'}}/>
-      <Entity light={{type: 'directional', intensity: 0.5}} position='-1 1 0'/>
-      <Entity light={{type: 'directional', intensity: 1}} position='1 1 0'/>
+            position="-2. 1.96 -2.7"
+            rotation="0 49.6 0"
+            
+            scale='0 0 0'
+            header='Moor Fountain'
+            headerAdjust='-1.5' // lower moves it to the left, higher to the right
+            text={this.state.allParagraphs['Fontana_del_Moro']}
+            textAdjust='0' //lower moves this down, higher moves this up
+            imageSrc='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Moor_Fountain.jpg/1024px-Moor_Fountain.jpg'
+          />
+          : null
+        }
+
+        <a-sky id="image-360" radius="30" src='#rome'></a-sky>
+
+        <Entity light={{type: 'ambient', color: '#888'}}/>
+        <Entity light={{type: 'directional', intensity: 0.5}} position='-1 1 0'/>
+        <Entity light={{type: 'directional', intensity: 1}} position='1 1 0'/>
 
       </Entity>
     );
