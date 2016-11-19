6a65464c938737d7f65142396a3dd0cce3ad2d4b
Carlos Portillo <caportil@gmail.com>
2016-11-18 17:16:02 -0800

    Integrate algorithm placing info cards in correct XYZ/rotation coordinates

--- react-client/src/components/Louvre.js
+++ react-client/src/components/Louvre.js
@@ -30,24 +30,24 @@ differently but that's how I've always rendered new components in React before
 */
 
 //temporary workaround for now
-// let showPyramid = false;
+// let showPyramidCard = false;
 
 // export default this.props => (
 class Louvre extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showPyramid: false,
-      showLeftWingPlane: false,
-      allTitles: ['Louvre_Pyramid', 'Louvre']
-    };
-  }
+      showPyramidCard: false,
+      showLeftWingCard: false,
+      showMuseumCard: false,
+      showCourtyardCard: false
+    }
 
   componentWillMount () {
     this.props.getParagraph(this.state.allTitles, allParagraphs => {
       this.setState( {allParagraphs: allParagraphs} );
-      console.log('🍊  this.state allParagraphs', this.state.allParagraphs);
-    });
+      console.log('🍊 this.state allParagraphs', this.state.allParagraphs);
+    }
   }
 
   render () {
@@ -65,7 +65,7 @@ class Louvre extends React.Component {
 
         <Entity id="pyramidTag"
             onClick={() => {
-              self.setState({showPyramid: true});
+              self.setState({showPyramidCard: true});
             }}
             geometry="primitive: plane; width: 2; height: 2"
             material={{src: 'url(https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-cubes.jpg)', opacity: 0.85}}
@@ -74,11 +74,11 @@ class Louvre extends React.Component {
         >
         </Entity>
 
-        {self.state.showPyramid ?
-
-            <TextPlane
+        {self.state.showPyramidCard? 
+          
+            <TextPlane 
               id="pyramidTextPlane"
-              hidePlane={() => self.setState({showPyramid: false})}
+              hidePlane={() => self.setState({showPyramidCard: false})}
 
               position="-4.34 2.22 6.83"
               rotation="8.02 135.22 -1.15"
@@ -111,10 +111,10 @@ class Louvre extends React.Component {
         />
 */}
 
-        <Entity id="leftWingTag"
+        <Entity id="richelieuWingTag" 
           geometry="primitive: plane; width: 2; height: 2"
           onClick={() => {
-            self.setState({showLeftWingPlane: true});
+            self.setState({showLeftWingCard: true});
           }}
           material={{color: 'yellow', opacity: 0.35}}
           position="-8.02 2.97 -3.8"
@@ -122,11 +122,11 @@ class Louvre extends React.Component {
         >
         </Entity>
 
-        {self.state.showLeftWingPlane ?
+        {self.state.showLeftWingCard?
 
-          <TextPlane
-            id="leftWingTagPlane"
-            hidePlane={() => self.setState({showLeftWingPlane: false})}
+          <TextPlane 
+            id="richelieuWingCard"
+            hidePlane={() => self.setState({showLeftWingCard: false})}
 
             position="-8.02 2.97 -3.8"
             rotation="-2.86 53.86 2.86"
@@ -146,23 +146,68 @@ class Louvre extends React.Component {
 
         <Entity id='courtyardTag'
             geometry="primitive: plane; width: 5; height: 5"
-            onClick={() => console.log('The Napoleonic Courtyard in Paris, France')}
+            onClick={() => self.setState({showCourtyardCard: true})}
             material={{color: 'yellow', opacity: 0.35}}
             position="6.4 -4 0.11"
-            rotation="-89.46 -89.95 85">
+            rotation="-89.46 -89.95 85"
+        >
         </Entity>
 
-        <Entity id="richelieuWingTag"
+        {self.state.showCourtyardCard?
+
+          <TextPlane 
+            id="courtyardCard"
+            hidePlane={() => self.setState({showCourtyardCard: false})}
+
+            position="6.4 -4 0.11"
+            rotation="-2.86 53.86 2.86"
+            
+            scale='0 0 0'
+            header='Louvre Museum'
+            headerAdjust='-1.5' // lower moves it to the left, higher to the right
+            text= {`The Louvre or the Louvre Museum (French: Musée du Louvre, pronounced: [myze dy luvʁ]) (French   ) is the world's largest museum and a historic monument in Paris, France. A central landmark of the city, it is located on the Right Bank of the Seine in the city's 1st arrondissement (district or ward). Nearly 35,000 objects from prehistory to the 21st century are exhibited over an area of 72,735 square metres (782,910 square feet). The Louvre is the world's second most visited museum after the Palace Museum in China, receiving more than 9.26 million visitors in 2014.`}
+            textAdjust='0' //lower moves this down, higher moves this up
+            imageSrc='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Le_Louvre_-_Aile_Richelieu.jpg/800px-Le_Louvre_-_Aile_Richelieu.jpg '
+          />
+
+          :
+
+          null
+        }
+
+        <Entity id="museumTag" 
             geometry="primitive: plane; width: 2; height: 2"
             draw="text: Hello"
-            onClick={() => this.props.changePlaneColor()}
+            onClick={() => self.setState({showMuseumCard: true})}
             material={{color: this.props.planeColor[this.props.colorIndex], opacity: 0.25}}
-            position="2.32 3.77 8.53"
-            rotation="20.63 -170.74 -6.88"
+            position="7 3 -5.42"
+            rotation="18.91 -52.14 0"
             look-at="[camera]"
             // lookAt={[camera]}
         >
         </Entity>
+
+        {self.state.showMuseumCard?
+
+          <TextPlane 
+            id="museumCard"
+            hidePlane={() => self.setState({showMuseumCard: false})}
+
+            position="7 3 -5.42"
+            rotation="18.91 -52.14 0"
+            
+            scale='0 0 0'
+            header='Louvre Museum'
+            headerAdjust='-1.5' // lower moves it to the left, higher to the right
+            text= {`The Louvre or the Louvre Museum (French: Musée du Louvre, pronounced: [myze dy luvʁ]) (French   ) is the world's largest museum and a historic monument in Paris, France. A central landmark of the city, it is located on the Right Bank of the Seine in the city's 1st arrondissement (district or ward). Nearly 35,000 objects from prehistory to the 21st century are exhibited over an area of 72,735 square metres (782,910 square feet). The Louvre is the world's second most visited museum after the Palace Museum in China, receiving more than 9.26 million visitors in 2014.`}
+            textAdjust='0' //lower moves this down, higher moves this up
+            imageSrc='https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Vista_exterior_del_Museo_del_Louvre.JPG/1280px-Vista_exterior_del_Museo_del_Louvre.JPG'
+          />
+
+          :
+
+          null
+        }
 
     {/*Test TextPlane for Debugging (will display automatically in front of view upon rendering) */}
       {/*}
