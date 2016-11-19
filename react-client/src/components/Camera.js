63913b9444e3a58c8b903133911bb044318d337c
Franco Abaroa <franco9395@gmail.com>
2016-11-19 14:35:43 -0800

    Minor style changes

--- react-client/src/components/Camera.js
+++ react-client/src/components/Camera.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 export default props => (
   <Entity>
-    <Entity camera="" position="0 0 0" rotation="-17.3 26.3 0" look-controls="" wasd-controls="" {...props}/>
+    <Entity camera="" position="-0.68 0.28 -0.89" rotation="-21.081521447863395 -4.181354700959849 0" look-controls="" wasd-controls="" {...props}/>
+
   </Entity>
 );
