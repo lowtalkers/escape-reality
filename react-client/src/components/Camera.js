eef19273553fc825bd0743f9d8aa6b8d41b88ad7
Carlos Portillo <caportil@gmail.com>
2016-11-19 23:33:20 -0800

    Reset beginning view coordinates to center of sphere

--- react-client/src/components/Camera.js
+++ react-client/src/components/Camera.js
@@ -4,6 +4,5 @@ import React from 'react';
 export default props => (
   <Entity>
     <Entity camera="" position="0 0 -2" rotation="0 0 0" look-controls="" wasd-controls="" {...props}/>
-
   </Entity>
 );
