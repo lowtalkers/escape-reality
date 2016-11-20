3fdb15ebb9ff92a1c1e671f519af719ad0baf732
Franco Abaroa <franco9395@gmail.com>
2016-11-19 16:04:34 -0800

    New lobby icon images

--- react-client/src/components/Lobby.js
+++ react-client/src/components/Lobby.js
@@ -15,15 +15,16 @@ import Plane from './Plane';
 export default props => (
   <Entity>
 
-        <Entity rotation="0 30 0" position="0 0 -2" material={{src: '#milan2', color: '#FFF', shader: 'flat', side: 'double', transparent: 'true', repeat: '-1 1'}} geometry="height:1;primitive:cylinder;radius:2;segmentsRadial:48;thetaLength:30;openEnded:true;thetaStart:0" onClick={() => props.router.replace('/milan')}/>
+        <Entity rotation="0 230 0" position="0 0 -2" material={{src: '#milan2', color: '#FFF', shader: 'flat', side: 'double', transparent: 'true', repeat: '-1 1'}} geometry="height:1;primitive:cylinder;radius:2;segmentsRadial:48;thetaLength:30;openEnded:true;thetaStart:0" onClick={() => props.router.replace('/milan')}/>
+
+        <Entity rotation="0 180 0" position="0 0 -2" material={{src: '#sf1', color: '#FFF', shader: 'flat', side: 'double', transparent: 'true', repeat: '-1 1'}} geometry="height:1;primitive:cylinder;radius:2;segmentsRadial:48;thetaLength:30;openEnded:true;thetaStart:0" onClick={() => props.router.replace('/sf')}/>
+
+
+        <Entity rotation="0 130 0" position="0 0 -2" material={{src: '#berlin2', color: '#FFF', shader: 'flat', side: 'double', transparent: 'true', repeat: '-1 1'}} geometry="height:1;primitive:cylinder;radius:2;segmentsRadial:48;thetaLength:30;openEnded:true;thetaStart:0" onClick={() => props.router.replace('/berlin')}/>
 
         <Entity rotation="0 80 0" position="0 0 -2" material={{src: '#paris', color: '#FFF', shader: 'flat', side: 'double', transparent: 'true', repeat: '-1 1'}} geometry="height:1;primitive:cylinder;radius:2;segmentsRadial:48;thetaLength:30;openEnded:true;thetaStart:0" onClick={() => props.router.replace('/louvre')}/>
 
-        <Entity rotation="0 130 0" position="0 0 -2" material={{src: '#sf1', color: '#FFF', shader: 'flat', side: 'double', transparent: 'true', repeat: '-1 1'}} geometry="height:1;primitive:cylinder;radius:2;segmentsRadial:48;thetaLength:30;openEnded:true;thetaStart:0" onClick={() => props.router.replace('/sf')}/>
-
-        <Entity rotation="0 180 0" position="0 0 -2" material={{src: '#berlin2', color: '#FFF', shader: 'flat', side: 'double', transparent: 'true', repeat: '-1 1'}} geometry="height:1;primitive:cylinder;radius:2;segmentsRadial:48;thetaLength:30;openEnded:true;thetaStart:0" onClick={() => props.router.replace('/berlin')}/>
-
-        <Entity rotation="0 230 0" position="0 0 -2" material={{src: '#rome2', color: '#FFF', shader: 'flat', side: 'double', transparent: 'true', repeat: '-1 1'}} geometry="height:1;primitive:cylinder;radius:2;segmentsRadial:48;thetaLength:30;openEnded:true;thetaStart:0" onClick={() => props.router.replace('/rome')}/>
+        <Entity rotation="0 30 0" position="0 0 -2" material={{src: '#rome2', color: '#FFF', shader: 'flat', side: 'double', transparent: 'true', repeat: '-1 1'}} geometry="height:1;primitive:cylinder;radius:2;segmentsRadial:48;thetaLength:30;openEnded:true;thetaStart:0" onClick={() => props.router.replace('/rome')}/>
 
 
       <Entity light={{type: 'ambient', color: '#fff'}}/>
