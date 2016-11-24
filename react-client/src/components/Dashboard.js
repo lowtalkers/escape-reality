935d192a9e81ece0987ee66aae158a0c11a28ae7
Franco Abaroa <franco9395@gmail.com>
2016-11-23 16:54:33 -0800

    Add the ability to send picture info to DB and retrieve top pictures from server

--- react-client/src/components/Dashboard.js
+++ react-client/src/components/Dashboard.js
@@ -1,20 +1,58 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
 import $ from 'jquery';
+var fecha = require('fecha');
 var vex = require('vex-js');
 vex.registerPlugin(require('vex-dialog'));
 vex.defaultOptions.className = 'vex-theme-os';
 
 var fileName;
 var filePath;
+var picDescrip;
+var date = fecha.format(new Date(), 'mediumDate').replace(/\s+/g, '');
+var currentImgs = [];
+
+
+const getAllPhotos = () => {
+  $.get({
+    url: '/topPics',
+    success: (data) => {
+      console.log(data);
+    },
+    error: (error) => {
+      console.error('error in get upload', error);
+      $('.error').show();
+    },
+  });
+};
+
+const savePhoto = (data) => {
+  var link = 'https://s3.amazonaws.com/' + data.Bucket + '/' + data.Key;
+  console.log(link);
+  $.post({
+    url: '/addPic',
+    data: JSON.stringify({title: data.Key, imageLink: link, description: picDescrip}),
+    contentType: 'application/json',
+    success: (data) => {
+      console.log(data);
+      getAllPhotos();
+    },
+    error: (error) => {
+      console.error('error in get upload', error);
+      $('.error').show();
+    },
+  });
+};
 
 const uploadFile = () => {
 
   $(function () {
-    $(":file").change(function () {
+    $("#fileUp").change(function () {
       if (this.files && this.files[0]) {
         var reader = new FileReader();
-        fileName = this.files[0].name;
+        picDescrip = $("#description").val();
+        fileName = date + this.files[0].name;
+        console.log(fileName);
         reader.onload = imageIsLoaded;
         reader.readAsDataURL(this.files[0]);
       }
@@ -25,20 +63,24 @@ const uploadFile = () => {
     $('#myImg').attr('src', e.target.result);
     filePath = e.target.result;
 
-    $.post({
-      url: '/upload',
-      data: JSON.stringify({fileName: fileName, filePath: filePath}),
-      contentType: 'application/json',
-      success: (data) => {
-        console.log(data);
-        $("#fileUp").val('');
-        vex.dialog.alert('Image uploaded!');
-      },
-      error: (error) => {
-        console.error('error in get upload', error);
-        $('.error').show();
-      },
-    });
+    if (!currentImgs.includes(fileName)) {
+      currentImgs.push(fileName);
+      $.post({
+        url: '/upload',
+        data: JSON.stringify({fileName: fileName, filePath: filePath}),
+        contentType: 'application/json',
+        success: (data) => {
+          console.log(data);
+          $("#fileUp").val('');
+          vex.dialog.alert('Image uploaded!');
+          savePhoto(data);
+        },
+        error: (error) => {
+          console.error('error in get upload', error);
+          $('.error').show();
+        },
+      });
+    }
   };
 }
 
@@ -70,8 +112,12 @@ export default props => {
           <h1>Explore!</h1>
         </button>
       </Link>
+      <div>
+        <input type='file' id='fileUp' />
 
-      <input type='file' id='fileUp' />
+        <label htmlFor='description'> A nice description of the photo</label>
+        <input type='text' id='description'/>
+      </div>
 
       <Link to="/signin">
         <button onClick={() => {signOut()}}>
