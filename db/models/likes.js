d2c03e57716f692f3356c147a5a0f414435221ca
Franco Abaroa <franco9395@gmail.com>
2016-11-29 09:55:03 -0800

    Add column for like (boolean)

--- db/models/likes.js
+++ db/models/likes.js
@@ -5,8 +5,15 @@ module.exports = (sequelize, Sequelize) => {
       primaryKey: true,
       allowNull: false,
       autoIncrement: true
+    },
+    like: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
     }
   });
 
   return Like;
+
 };
+
+//find user id and find like where user id is equal to user id
