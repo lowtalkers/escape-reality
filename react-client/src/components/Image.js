c8cf9d3a151f4e1f4ddd6202d8652e83c4e617d1
Franco Abaroa <franco9395@gmail.com>
2016-11-29 19:41:52 -0800

    Add functions for recording comments, saving likes

--- react-client/src/components/Image.js
+++ react-client/src/components/Image.js
@@ -19,36 +19,23 @@ import Plane from './Plane';
 export default props => {
   var commentCoords;
   props.getComments();
-  
   return ( <Entity>
     <a-image id="close-image" src="#close" geometry="height: 0.3; width: 0.3" position="0 0 -2" onClick={() => {props.router.replace('/lobby'); props.changeBigPic('')}}></a-image>
 
-    <a-image id="commentStart" src="#commentStart" geometry="height: 0.3; width: 0.3" position="0 0.82 -3" onClick={()=> {if (annyang) {
-                var commands = {
-                'hello': function() { alert('Hello world!'); }
-              };
+    <a-image id="commentStart" src="#commentStart" geometry="height: 0.3; width: 0.3" position="0 0.82 -3" onClick={()=> {props.voiceComment()}}></a-image>
 
-                annyang.addCallback('result', function(phrases) {
-                console.log(phrases, phrases[0]);
-                alert(phrases[0]);
-              });
+    <a-image id="commentStop" src="#commentStop" geometry="height: 0.3; width: 0.3" position="0 0.42 -3" onClick={()=> {props.stopVoiceComment()}}></a-image>
 
-                annyang.addCommands(commands);
-                annyang.start();
-              }}}></a-image>
-
-    <a-image id="commentStop" src="#commentStop" geometry="height: 0.3; width: 0.3" position="0 0.42 -3" onClick={()=> {annyang.abort();}}></a-image>
-
-    <a-image id="like" src="#like" geometry="height: 0.3; width: 0.3" position="0 0 -3" onClick={() => props.likeSubmitFn()}></a-image>
+    <a-image id="like" src="#like" geometry="height: 0.3; width: 0.3" position="0 -0.50 -3" onClick={() => props.likeSubmitFn()}></a-image>
 
     <a-sky id="image-360" radius="10" src={'#'+props.bigPic.split('.')[0]}></a-sky>
 
+    <Entity
 
-    <Entity 
       rotation="0 180 0"
       position="0 0 0"
-      material={{opacity: '0', shader: 'flat', side: 'double', transparent: 'true', repeat: '-1 1'}} 
-      geometry="height:5;primitive:sphere;radius:7.5;segmentsRadial:48;thetaLength:360;openEnded:true;thetaStart:0" 
+      material={{opacity: '0', shader: 'flat', side: 'double', transparent: 'true', repeat: '-1 1'}}
+      geometry="height:5;primitive:sphere;radius:7.5;segmentsRadial:48;thetaLength:360;openEnded:true;thetaStart:0"
       onRaycasterIntersected={(obj) => {commentCoords = obj.detail.intersection.point}}
       onClick={() => {
         console.log(commentCoords);
