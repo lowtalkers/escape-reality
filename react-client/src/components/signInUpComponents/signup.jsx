68f5549575a6f775fd678a2fadd99a1a1d77770d
Franco Abaroa <franco9395@gmail.com>
2016-11-30 18:54:30 -0800

    Overhaul UI

--- react-client/src/components/signInUpComponents/signup.jsx
+++ react-client/src/components/signInUpComponents/signup.jsx
@@ -1,17 +1,62 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const style = {
+  fontFamily: 'Orbitron'
+};
 
 export default props => (
 
   <div className="signup">
-    <h1> Escape </h1>
-    <div className="signintitle"> Signup </div>
+    <h1 className="centerText"> ImmerseVR </h1>
+
+    <div className="centerButtons">
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <TextField
+      hintText="Email Field"
+      floatingLabelText="Email"
+      onChange={event => props.onEmailChange(event)}
+    />
+    </MuiThemeProvider>
+    <br />
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <TextField
+      hintText="Password Field"
+      floatingLabelText="Password"
+      type="password"
+      onChange={event => props.onPasswordChange(event)}
+    />
+    </MuiThemeProvider>
+    <br />
+    <br/>
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <RaisedButton buttonStyle={style} label="Sign Up" primary={true} onClick={props.submitFn} />
+    </MuiThemeProvider>
+
+    <br/>
+    <br/>
+
+    <div>
+        <p className="fancyText"> Already have an account? </p>
+        <Link to="/signin" className="fancyText"> Sign in here</Link>
+    </div>
+    </div>
+
+
+    {/*}
     <label htmlFor="email"> Email </label>
     <input type="text" id="email" name="email" onChange={event => props.onEmailChange(event)} />
     <label htmlFor="password"> Password </label>
     <input type="password" id="password" name="password" onChange={event => props.onPasswordChange(event)} />
-    <input type="button" id="signup" value="Signup" onClick={props.submitFn} />
-    Already have an account?
-    <Link to="/signin"> Sign in here</Link>
+    <input type="button" id="signin" value="Signin" onClick={props.submitFn} />
+    Don't have an account?
+
+    */}
+
   </div>
 );
+
