2924c212e95aba57ec4f83ba3c6b6ddd3ded844d
Franco Abaroa <franco9395@gmail.com>
2016-11-30 18:54:30 -0800

    Overhaul UI

--- react-client/src/components/Dashboard.js
+++ react-client/src/components/Dashboard.js
@@ -1,6 +1,58 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
 import $ from 'jquery';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import CircularProgress from 'material-ui/CircularProgress';
+import LinearProgress from 'material-ui/LinearProgress';
+import FlatButton from 'material-ui/FlatButton';
+
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+
+import Avatar from 'material-ui/Avatar';
+
+import {
+  blue300,
+  indigo900,
+  orange200,
+  deepOrange300,
+  pink400,
+  purple500,
+} from 'material-ui/styles/colors';
+
+const styles = {
+  button: {
+    margin: 12,
+    fontFamily: 'Orbitron !important',
+    backgroundColor: '#e8e8e8',
+    color: 'black !important'
+  },
+  exampleImageInput: {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    width: '100%',
+    opacity: 0,
+  },
+  exploreButton: {
+    fontFamily: 'Orbitron',
+  },
+};
+
+const style = {
+  margin: 12,
+};
+
+const injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
 
 const fecha = require('fecha');
 const vex = require('vex-js');
@@ -27,7 +79,7 @@ const getAllPhotos = () => {
   });
 };
 
-const uploadFile = () => {
+const uploadFile = (props) => {
   $(function () {
     const imageIsLoaded = e => {
       $('#myImg').attr('src', e.target.result);
@@ -44,10 +96,10 @@ const uploadFile = () => {
           }),
           contentType: 'application/json',
           success: data => {
+            props.uploadBar();
             console.log('Found user\'s uploaded photo from DB', data);
-            $('#fileUp').val('');
             $('#profilePicUp').val('');
-            vex.dialog.alert('Image uploaded!');
+            vex.dialog.alert('Uploaded!');
           },
           error: error => {
             console.error('error in get upload', error);
@@ -58,12 +110,14 @@ const uploadFile = () => {
     };
 
     $('#fileUp').change(function () {
+      console.log('file ATTEMPT');
       if (this.files && this.files[0]) {
         const reader = new FileReader();
         picDescrip = $('#description').val();
         fileName = this.files[0].name;
         console.log(fileName);
         reader.onload = imageIsLoaded;
+        props.uploadBar();
         reader.readAsDataURL(this.files[0]);
       }
     });
@@ -82,10 +136,12 @@ const uploadFile = () => {
 };
 
 const signOut = () => {
+  console.log('in sign out function');
   $.get({
     url: '/signOut',
     success: (data) => {
       console.log(data);
+      window.location.reload();
     },
     error: (error) => {
       console.error('error in get bookmarks', error);
@@ -94,40 +150,60 @@ const signOut = () => {
   });
 };
 
+
 export default props => {
-  uploadFile();
+  uploadFile(props);
 
   return (
     <div>
-      <p></p>
-      <Link to="/bookmarks">
-        <button>
-          <h1>Bookmarks</h1>
-        </button>
-      </Link>
 
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <Toolbar >
+        <ToolbarGroup>
+          <Avatar src="https://s3.amazonaws.com/vrpics/franco-profilepic_256x256.jpg" />
+        </ToolbarGroup>
+        <ToolbarGroup>
+          <ToolbarTitle className="centerText" text="Welcome, Franco" />
+
+        </ToolbarGroup>
+        <ToolbarGroup>
+          <IconMenu
+            iconButtonElement={
+              <IconButton touch={true}>
+                <NavigationExpandMoreIcon />
+              </IconButton>}>
+            <MenuItem primaryText="Account Settings" />
+            <MenuItem onClick={() => { signOut(); }} primaryText="Sign Out" />
+          </IconMenu>
+          </ToolbarGroup>
+      </Toolbar>
+      </MuiThemeProvider>
+
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+          <LinearProgress className={props.shouldHide ? 'hidden' : ''} mode="indeterminate" />
+      </MuiThemeProvider>
+
+      <h1 className="centerText"> ImmerseVr </h1>
+
+      <div className="centerButtons">
       <Link to="/lobby">
-        <button>
-          <h1>Explore!</h1>
-        </button>
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <RaisedButton buttonStyle={styles.exploreButton} label="Explore" primary={true}  />
+      </MuiThemeProvider>
       </Link>
-      <div>
-        <input type='file' id='fileUp' />
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
 
-        <label htmlFor='description'> A nice description of the photo</label>
-        <input type='text' id='description'/>
+      <RaisedButton
+      label="Upload"
+      labelPosition="before"
+      style={styles.button}
+      containerElement="label"
+      buttonStyle={styles.exploreButton}>
+      <input id="fileUp" type="file" style={styles.exampleImageInput} />
+      </RaisedButton>
+      </MuiThemeProvider>
       </div>
 
-      <Link to="/signin">
-        <button onClick={() => { signOut(); }}>
-          <div>Sign Out</div>
-        </button>
-      </Link>
-
-      <div>
-        <p>Profile Pic</p>
-        <p><img src={props.profilePic} width="200" height="200" /></p> 
-      </div>
     </div>
   );
 };
--- react-client/src/components/signInUpComponents/signin.jsx
+++ react-client/src/components/signInUpComponents/signin.jsx
@@ -1,17 +1,61 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const style = {
+  fontFamily: 'Orbitron'
+};
 
 export default props => (
 
   <div className="signin">
-    <h1> Escape </h1>
-    <div className="signintitle"> Signin </div>
+    <h1 className="centerText"> ImmerseVR </h1>
+
+    <div className="centerButtons">
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <TextField
+      hintText="Email Field"
+      floatingLabelText="Email"
+      onChange={event => props.onEmailChange(event)}
+    />
+    </MuiThemeProvider>
+    <br />
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <TextField
+      hintText="Password Field"
+      floatingLabelText="Password"
+      type="password"
+      onChange={event => props.onPasswordChange(event)}
+    />
+    </MuiThemeProvider>
+    <br />
+    <br/>
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <RaisedButton buttonStyle={style} label="Sign In" primary={true} onClick={props.submitFn} />
+    </MuiThemeProvider>
+
+    <br/>
+    <br/>
+
+    <div>
+        <p className="fancyText"> No account yet? </p>
+        <Link to="/signup" className="fancyText"> Sign up here</Link>
+    </div>
+    </div>
+
+
+    {/*}
     <label htmlFor="email"> Email </label>
     <input type="text" id="email" name="email" onChange={event => props.onEmailChange(event)} />
     <label htmlFor="password"> Password </label>
     <input type="password" id="password" name="password" onChange={event => props.onPasswordChange(event)} />
     <input type="button" id="signin" value="Signin" onClick={props.submitFn} />
     Don't have an account?
-    <Link to="/signup"> Sign up here</Link>
+
+    */}
+
   </div>
 );
