c937bd9464e7f2414f9c5c9147ab5e1c4467804f
spiritphyz <tonyle@ocf.berkeley.edu>
2017-01-27 16:40:29 -0800

    Create guest user
    - Note: if you already have a database running on your development
    environment, make user 'id=5' is free in the users table for the 'escape'
    database.

--- db/sql-to-mock-database.sql
+++ db/sql-to-mock-database.sql
@@ -73,6 +73,15 @@ VALUES
 
 
 -- ---
+-- Create guest user
+-- 
+-- ---
+INSERT INTO `users` (`id`, `email`, `password`, `profilePic`, `firstName`, `lastName`, `lobbyPic`, `createdAt`, `updatedAt`)
+VALUES
+  (5, 'guest@guest.com', '$2a$10$wBh6z7UpGPGcX9wn7Pv/6.nkcHsZ1Aq5ZeTWLrpDf205KmKBfToxC', 'http://i.imgur.com/aazLzwo.jpg', 'Guest', '', 'https://s3.amazonaws.com/vrpics/lr2.jpg', '2016-11-30 00:35:49', '2016-11-30 00:35:49');
+  
+
+-- ---
 -- Create comments
 -- 
 -- ---
